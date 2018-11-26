242d62d073e7b145f3e88800c7045dfd1de5083f
HubertFraczek <hhubcio12@gmail.com>
2018-11-26 20:10:29 +0100

    Added SFML and .gitignore files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    RenderWindow window(sf::VideoMode(800, 600), "Hello SFML!");
+    window.setFramerateLimit(60);
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        shape.move(1.0f, 1.0f);
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
