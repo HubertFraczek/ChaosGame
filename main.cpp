381bfeeae268808a1e848306d4092c98fb5d2b7d
HubertFraczek <hhubcio12@gmail.com>
2018-11-26 21:17:19 +0100

    ChaosGame class

--- main.cpp
+++ main.cpp
@@ -1,40 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "InitialTriangle.h"
-
-#define fps 60
-#define width 1600
-#define height 900
+#include "ChaosGame.h"
 
 using namespace sf;
 
 int main() {
 
-    RenderWindow window(sf::VideoMode(width, height), "Chaos Game");
-    window.setFramerateLimit(fps);
-
-    CircleShape shape(10.f);
-    shape.setFillColor(Color::Green);
-
-    InitialTriangle *triangle = new InitialTriangle();
-    //testing
-    std::cout << triangle->getAx() << "  " << triangle->getAy() << std::endl;
-    std::cout << triangle->getBx() << "  " << triangle->getBy() << std::endl;
-    std::cout << triangle->getCx() << "  " << triangle->getCy() << std::endl;
-
-    while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-            }
-        }
-
-        window.clear();
-        shape.move(1.0f, 1.0f);
-        window.draw(shape);
-        window.display();
-    }
+    ChaosGame *chaosGame = new ChaosGame();
+    chaosGame->run();
 
     return 0;
 }
